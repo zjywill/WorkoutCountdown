afc5b00c662d0a00ad75c53784cdcf3001e7d7ca
zjywill <zjywill@hotmail.com>
2014-11-17 16:48:15 +0800

    Initial commit

---
+++ app/src/main/java/com/comic/workoutcountdown/MainActivity.java
@@ -0,0 +1,51 @@
+package com.comic.workoutcountdown;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+
+public class MainActivity extends Activity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private RecyclerView.ItemDecoration mItemDecoration;
+    private ListCardAdapter mAdapter;
+
+    private String[] mData = {"a","b","a","b","a","b","a","b","a","b","a","b","a","b","a","b","a","b","a","b","a","b","a","b","a","b"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.section_list);
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mItemDecoration = new DividerDecoration(this);
+        mRecyclerView.addItemDecoration(mItemDecoration);
+
+        mAdapter = new ListCardAdapter(mData);
+        mRecyclerView.setAdapter(mAdapter);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
